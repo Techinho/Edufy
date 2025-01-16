ada4231bb50c18413cb91e1bc5ed689538ce80f9
ilyass <ilyassezzaouya4@gmail.com>
2025-01-16 23:23:10 +0100

    add pro sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+
+import { Link } from 'react-router-dom';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+
+
+
+const Sidebars = () => {
+  return (
+
+    <Sidebar>
+      <Menu>
+        <SubMenu label="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+      </Menu>
+    </Sidebar>
+  )
+}
+
+export default Sidebars
