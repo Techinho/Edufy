cc7dbfcb27855e088bd0a8d2f29a2b164bca2f98
Ezzaouya_Ilyass <156593840+Techinho@users.noreply.github.com>
2025-02-25 23:54:14 +0000

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -14,10 +14,7 @@ const Signup = () => {
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-6 shadow-lg rounded-lg sm:px-10">
           <SignupForm />
-          <p className="mt-4 text-center text-sm text-gray-600">
-            Already have an account? 
-            <Link to="/login" className="font-medium text-blue-600 hover:text-blue-500"> Sign in</Link>
-          </p>
+          
         </div>
       </div>
     </div>
